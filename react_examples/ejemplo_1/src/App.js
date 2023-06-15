1e616ee0902aca54361196a77c10dabffef155fd
yurley <yurs.ksf1@gmail.com>
2023-06-14 20:22:12 -0500

    definition more components

--- react_examples/ejemplo_1/src/App.js
+++ react_examples/ejemplo_1/src/App.js
@@ -2,21 +2,8 @@ import React, { useState } from 'react';
 
 // Componente Pizza
 const Pizza = ({ nombre, ingredientes, precio }) => {
-  return (
-    <div>
-      <h2>{nombre}</h2>
-      <p>Ingredientes: {ingredientes}</p>
-      <p>Precio: ${precio}</p>
-    </div>
-  );
-};
-
-// Componente principal
-const App = () => {
-  // Estado para la cantidad de rebanadas
   const [cantidadRebanadas, setCantidadRebanadas] = useState(8);
 
-  // Función para comer una rebanada
   const comerRebanada = () => {
     if (cantidadRebanadas > 0) {
       setCantidadRebanadas(cantidadRebanadas - 1);
@@ -25,10 +12,22 @@ const App = () => {
 
   return (
     <div>
-      <h1>¡Bienvenido a nuestra pizzería!</h1>
-      <Pizza nombre="Pizza de queso" ingredientes="Queso, salsa de tomate" precio={12} />
+      <h2>{nombre}</h2>
+      <p>Ingredientes: {ingredientes}</p>
+      <p>Precio: ${precio}</p>
       <p>Cantidad de rebanadas restantes: {cantidadRebanadas}</p>
       <button onClick={comerRebanada}>Comer una rebanada</button>
+    </div>
+  );
+};
+
+// Componente principal
+const App = () => {
+  return (
+    <div>
+      <h1>¡Bienvenido a nuestra pizzería!</h1>
+      <Pizza nombre="Pizza de queso" ingredientes="Queso, salsa de tomate" precio={12} />
+      <Pizza nombre="Pizza de pepperoni" ingredientes="Queso, salsa de tomate, pepperoni" precio={14} />
     </div>
   );
 };
